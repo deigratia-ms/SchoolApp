c91b18360c51f9e557750bad0f74838b8f984ef9
deigratia-ms <deigratia.ms@example.com>
2025-06-07 13:31:21 +0000

    Fix Django template configuration conflict
    - Disable APP_DIRS when using custom loaders in production
    - Resolves ImproperlyConfigured error: 'app_dirs must not be set when loaders is defined'
    - This fixes the template loading issue on PythonAnywhere when DEBUG=False

--- ricas_school_manager/settings.py
+++ ricas_school_manager/settings.py
@@ -251,6 +251,8 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 
 # Template caching (only for production)
 if not DEBUG:
+    # Disable APP_DIRS when using custom loaders
+    TEMPLATES[0]['APP_DIRS'] = False
     TEMPLATES[0]['OPTIONS']['loaders'] = [
         ('django.template.loaders.cached.Loader', [
             'django.template.loaders.filesystem.Loader',
